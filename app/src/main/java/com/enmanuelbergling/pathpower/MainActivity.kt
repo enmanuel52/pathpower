efa217f73ae8e4aded18ea7ce7c37a60940d788c
enmanuel <enmanueldelgado52@gmail.com>
2025-12-15 23:13:56 -0300

    test publishing

--- app/src/main/java/com/enmanuelbergling/pathpower/MainActivity.kt
+++ app/src/main/java/com/enmanuelbergling/pathpower/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,7 +20,6 @@ class MainActivity : ComponentActivity() {
             PathPowerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,9 +29,8 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Greeting(modifier: Modifier = Modifier) {
+    ImagePreview(
         modifier = modifier
     )
 }
@@ -42,6 +39,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PathPowerTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
